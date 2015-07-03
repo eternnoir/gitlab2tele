9402e7a25b6458aaeb23ee5ba1a0e1d496572d26
eternnoir <eternnoir@gmail.com>
2015-07-03 13:28:42 +0800

    Add merge request support.

--- gitlab2tele/__init__.py
+++ gitlab2tele/__init__.py
@@ -8,11 +8,13 @@ class TeleSender():
         self.bot = telebot.TeleBot(token)
         self.chat_id = chat_id
 
-    def post_project_event(self, json_string):
-        obj = json_string
+    def post_project_event(self, json_obj):
+        obj = json_obj
         event_type = obj['object_kind']
         if event_type == 'push':
             self.__revice_push(obj)
+        if event_type == 'merge_request':
+            self.__revice_merge_request(obj)
 
     def __revice_push(self, json_obj):
         msg = self.__parse_push_event(json_obj)
@@ -22,6 +24,18 @@ class TeleSender():
         push_user = json_obj['user_name']
         commit_count = json_obj['total_commits_count']
         repo_url = json_obj['repository']['homepage']
-        reop_name = json_obj['repository']['name']
-        ret = '%s push %d commits to %s. %s' % (push_user, commit_count, reop_name, repo_url)
+        repo_name = json_obj['repository']['name']
+        ret = '%s push %d commits to %s. %s' % (push_user, commit_count, repo_name, repo_url)
+        return ret
+
+    def __revice_merge_request(self, json_obj):
+        msg = self.__parse_merge_request(json_obj)
+        self.bot.send_message(self.chat_id, msg)
+
+    def __parse_merge_request(self, json_obj):
+        request_user = json_obj['user']['name']
+        mr_title = json_obj['object_attributes']['title']
+        url = json_obj['object_attributes']['url']
+        action = json_obj['object_attributes']['action']
+        ret = u'%s %s a merge request : %s. %s' % (request_user, action, mr_title, url)
         return ret
--- runserver.py
+++ runserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask,request
+from flask import Flask, request
 import gitlab2tele
 import logging
 
@@ -11,17 +11,18 @@ app.logger.addHandler(logging.StreamHandler())
 TOKEN = os.environ['TOKEN']
 CHAT_ID = int(os.environ['CHAT_ID'])
 app.logger.info('TOKEN:' + TOKEN)
-app.logger.info('CHAT_ID: '+str(CHAT_ID))
+app.logger.info('CHAT_ID: ' + str(CHAT_ID))
+
 
 @app.route("/gitlab/project", methods=["PUT", "POST"])
 def gitlab_project():
     json_obj = request.get_json()
     app.logger.info(json_obj)
-    ts = gitlab2tele.TeleSender(TOKEN,CHAT_ID)
+    ts = gitlab2tele.TeleSender(TOKEN, CHAT_ID)
     ts.post_project_event(json_obj)
     return 'OK'
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run('0.0.0.0', port=port,debug=True)
+    app.run('0.0.0.0', port=port, debug=True)
